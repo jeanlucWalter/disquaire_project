1b06600616400770b53718ac516577d0396b9c82
jeanluc <jeanluc.walter@fantasiapp.com>
2021-07-07 11:07:10 +0200

    Importation des fichiers statiques 2

--- store/views.py
+++ store/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 message = "Salut tout le monde !"
 
 def index(request):
-  albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
-  formattedAlbums = ["<li>{}</li>".format(album.title) for album in albums]
-  message = """<ul>{}</ul>""".format("\n".join(formattedAlbums))
-  return HttpResponse(message)
+  template = loader.get_template('store/index.html')
+  return HttpResponse(template.render(request=request))
+  # albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
+  # formattedAlbums = ["<li>{}</li>".format(album.title) for album in albums]
+  # message = """<ul>{}</ul>""".format("\n".join(formattedAlbums))
+  # return HttpResponse(message)
 
 def listing(request):
   albums = Album.objects.filter(available=True)
