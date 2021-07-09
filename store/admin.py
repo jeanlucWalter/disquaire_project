ad83b9796e893ae014cc0ff13c57cf26754d06bd
jeanluc <jeanluc.walter@fantasiapp.com>
2021-07-09 14:34:05 +0200

    fin des développements

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,69 @@
+import django
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+from .models import Booking, Contact, Album, Artist
+from django.contrib.contenttypes.models import ContentType
 
-# Register your models here.
+class AdminURLMixin(object):
+  def get_admin_url(self, obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return reverse("admin:store_{}_change".format(content_type.model), args=(obj.id,))
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin, AdminURLMixin):
+  fields = ["created_at", "contact_link", 'album_link', 'contacted']
+  readonly_fields = ['created_at', "contact_link", "album_link", "contacted"]
+  list_filter = ['created_at', 'contacted']
+
+  def has_add_permission(self, request) -> bool:
+    return False
+
+  def contact_link(self, booking):
+    url = self.get_admin_url(booking.contact)
+    return mark_safe("<a href='{}'>{}</a>".format(url, booking.contact.name))
+
+  contact_link.short_description = "Client"
+
+  def album_link(self, booking):
+    url = self.get_admin_url(booking.album)
+    return mark_safe("<a href='{}'>{}</a>".format(url, booking.album.title))
+
+  album_link.short_description = "Album"
+
+class BookingInline(admin.TabularInline, AdminURLMixin):
+  model = Booking
+  extra = 0
+  readonly_fields = ["created_at", "album_link", "contacted"]
+  fields = ["created_at", "album_link", "contacted"]
+  verbose_name = "Réservation"
+  verbose_name_plural = "Réservations"
+
+  def album_link(self, booking):
+    url = self.get_admin_url(booking.album)
+    return mark_safe("<a href='{}'>{}</a>".format(url, booking.album.title))
+
+  album_link.short_description = "Album"
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [BookingInline,]
+
+class AlbumArtistInline(admin.TabularInline):
+    model = Album.artists.through
+    extra = 1
+    verbose_name = "Disque"
+    verbose_name_plural = "Disques"
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [AlbumArtistInline,]
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ['reference', 'title']
+    inlines = [AlbumArtistInline,]
+    fieldsets = [
+        (None, {'fields': ['available', 'title']})
+        ]
--- store/models.py
+++ store/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Artist(models.Model):
-  name = models.CharField(max_length=200, unique=True)
+  name = models.CharField('nom', max_length=200, unique=True)
+
+  class Meta:
+    verbose_name = "artiste"
 
   def __str__(self) -> str:
     return self.name
@@ -10,25 +13,34 @@ class Contact(models.Model):
   email = models.EmailField(max_length=100)
   name = models.CharField(max_length=200)
 
+  class Meta:
+    verbose_name = "prospect"
+
   def __str__(self) -> str:
     return self.name
 
 class Album(models.Model):
-  reference = models.IntegerField(null=True)
-  created_at = models.DateTimeField(auto_now_add=True)
+  reference = models.IntegerField('référence', blank=True, null=True)
+  created_at = models.DateTimeField('date d\'achat', auto_now_add=True)
   available = models.BooleanField(default=True)
-  title = models.CharField(max_length=200)
+  title = models.CharField('titre', max_length=200)
   picture = models.URLField()
   artists = models.ManyToManyField(Artist, related_name='album', blank=True)
+
+  class Meta:
+    verbose_name = "disque"
 
   def __str__(self) -> str:
     return self.title
 
 class Booking(models.Model):
-  created_at = models.DateTimeField(auto_now_add=True)
-  contacted = models.BooleanField(default=False)
+  created_at = models.DateTimeField("date de réservation", auto_now_add=True)
+  contacted = models.BooleanField("demande traitée", default=False)
   contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
   album = models.OneToOneField(Album, on_delete=models.CASCADE)
+
+  class Meta:
+    verbose_name = "réservation"
 
   def __str__(self) -> str:
     return self.contact.name
